51f46401b7a873433d63e23428dfa782f61f4911
Luis Rishi Puno <LRPuno@gmail.com>
2018-12-20 07:00:00 -0500

    Setting the Stage

--- js/levels.js
+++ js/levels.js
@@ -236,6 +236,8 @@ brawl.state4.prototype = {
         this.game.physics.arcade.overlap(this.player, this.finish, nextLevel, null, this);
 
         this.player.body.velocity.x = 0;
+        this.player.body.acceleration.y = -650;
+        this.player.body.acceleration.x = -1000;
 
         ///Player Movement and Wall-Jump Mechanics
         if (this.player.body.touching.down) {
