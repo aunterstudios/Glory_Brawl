30ebb1c7ca007f8c7fb633a7c1e2d84995bb4ce2
Luis Rishi Puno <LRPuno@gmail.com>
2018-12-20 08:33:30 -0500

    Messing Around with Air Controls

--- js/levels.js
+++ js/levels.js
@@ -259,10 +259,10 @@ brawl.state4.prototype = {
             this.player.body.velocity.y = 50;
             this.player.frame = 6;
             if ((this.cursors.up.isDown && this.cursors.left.isDown) || (this.player.customParams.mustJump && this.player.customParams.leftMovement)) {
-                this.player.body.velocity.y = -800;
-                this.player.body.velocity.x = -500;
-                this.player.body.acceleration.y = -10000;
-                this.player.body.acceleration.x = -10000;
+                this.player.body.velocity.y = -500;
+                this.player.body.velocity.x = -50;
+                this.player.body.acceleration.y = -300;
+                this.player.body.acceleration.x = -50;
             }
         }
         else if (this.player.body.touching.left) {
@@ -281,11 +281,13 @@ brawl.state4.prototype = {
         else if (this.player.body.touching.none) {
             this.player.frame = 10;
             if (this.cursors.left.isDown || this.player.customParams.leftMovement) {
-                this.player.body.velocity.x = -400;
+                this.player.body.velocity.x = -600;
+                this.player.body.acceleration.x = -1000;
                 this.player.customParams.rightMovement = false;
             }
             else if (this.cursors.right.isDown || this.player.customParams.rightMovement) {
-                this.player.body.velocity.x = 400;
+                this.player.body.velocity.x = 600;
+                this.player.body.acceleration.x = 1000;
                 this.player.customParams.leftMovement = false;
             }
         }
