9d99cbd5a710c943d61d49c5edc494d86d48d6f1
Luis Rishi Puno <LRPuno@gmail.com>
2019-05-08 18:01:50 -0400

    enter to play force people to look at controls

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -100,7 +100,7 @@ brawl.stateControls.prototype = {
             else {
                 var xText = 1000;
                 var yText = 700;
-                var textInput = "Press Spacebar to Play";
+                var textInput = "Press Enter to Play";
                 var fontFill = "#ffffff";
                 var fontSize = 20;
             }
@@ -112,7 +112,7 @@ brawl.stateControls.prototype = {
         }
     },
     update: function () {
-        if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+        if (this.game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) {
             this.game.state.start('rogueTest');
         }
 
