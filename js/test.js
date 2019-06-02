9364e5fad3cbb0e9b877fb56dcca6f4397075ef3
Luis Rishi Puno <LRPuno@gmail.com>
2019-06-01 22:39:32 -0400

    better

--- js/test.js
+++ js/test.js
@@ -53,14 +53,40 @@ brawl.testing.prototype = {
         this.game.physics.arcade.OVERLAP_BIAS = 12;
 
         ////////////////////Game World Size//////////////////////
+        /*
+        Reference Point for Different World Creation
+        if (gridSystemGenesis >= 0 && gridSystemGenesis <= 41) {
+            this.wallSpawn(x, y, rect, positionInRectangle);
+        }
+        else if (gridSystemGenesis >= 42 && gridSystemGenesis <= 56) {
+            this.enemySpawn(x, y, rect, positionInRectangle);
+        }
+        else if (gridSystemGenesis >= 57 && gridSystemGenesis <= 61) {
+            this.ledgeSpawn(x, y, rect, positionInRectangle);
+        }
+        else if (gridSystemGenesis >= 62 && gridSystemGenesis <= 69) {
+            this.ledgeDownSpawn(x, y, rect, positionInRectangle);
+        }
+        else if (gridSystemGenesis >= 70 && gridSystemGenesis <= 74) {
+            this.ballSpawn(x, y, rect, positionInRectangle);
+        }
+        else if (gridSystemGenesis >= 75 && gridSystemGenesis <= 84) {
+            this.ledgeSideSpawn(x, y, rect, positionInRectangle);
+        }
+        else if (gridSystemGenesis >= 85 && gridSystemGenesis <= 100) {
+            this.spikeSpawn(x, y, rect, positionInRectangle);
+        }
+        */
         var randomGeneratorForWorld = this.game.rnd.integerInRange(0, 100);
-        if (randomGeneratorForWorld < 50) {
+        if (randomGeneratorForWorld >= 00 && randomGeneratorForWorld <= 50) {
+            //Standard Big World
             this.game.world.setBounds(0, 0, 2000, 2000);
-            console.log("2000");
+            console.log("2000X+2000Y");
         }
         else {
+            //Canvas Size but Tall?
             this.game.world.setBounds(0, 0, 1400, 2000);
-            console.log("1000");
+            console.log("1400X+1400Y");
         }
         //Keyboard Controls
         this.cursors = this.game.input.keyboard.createCursorKeys();
@@ -120,7 +146,7 @@ brawl.testing.prototype = {
             this.worldCreator(500, 700, 4, 1400, 1900, 400, 400, 500, 500, 4, 4, 0, 0, 3);
         }
         else {
-            this.worldCreator(200, 200, 2, 1400, 1900, 200, 200, 250, 250, 5, 7, 0, 0, 2);
+            this.worldCreator(200, 200, 2, 1400, 1900, 200, 200, 250, 250, 5, 7, 0, 0, 1);
         }
 
         // this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_PLATFORMER);
@@ -157,9 +183,9 @@ brawl.testing.prototype = {
     // //     // console.log(this.ledgeX.x + ' ' + this.ledgeX.y);
 
     // // },
-    //////////////Creation of the Grid System (Objects Spawning)///////////////
+    //////////////Creation of the World///////////////
     worldCreator: function (playerX, playerY, deathIterator, deathX, deathY, xBlockSizeF, yBlockSizeF, xRectangleF, yRectangleF, iteratorX, iteratorY, baseCampX, baseCampY, amountOfSpritesInGrid) {
-        console.log("amount of Sprites"+ amountOfSpritesInGrid);
+        console.log("amount of Sprites" + amountOfSpritesInGrid);
         //////////////////Player Position////////////////
         ////////////////////Adding Player//////////////////////
         this.player = this.game.add.sprite(playerX, playerY, 'dude');
@@ -287,9 +313,12 @@ brawl.testing.prototype = {
                             positionArray.push(bottomLeft);
                             positionArray.push(topRight);
                         }
-                        else {
+                        else if (amountOfSpritesInGrid === 2) {
                             positionArray.push(center);
                             positionArray.push(topRight);
+                        }
+                        else {
+                            positionArray.push(center);
                         }
                     }
                     else if (randomGeneratorForArray >= 26 && randomGeneratorForArray <= 50) {
@@ -298,9 +327,12 @@ brawl.testing.prototype = {
                             positionArray.push(bottomLeft);
                             positionArray.push(topRight);
                         }
-                        else {
+                        else if (amountOfSpritesInGrid === 2) {
                             positionArray.push(center);
                             positionArray.push(topRight);
+                        }
+                        else {
+                            positionArray.push(center);
                         }
                     }
                     else if (randomGeneratorForArray >= 51 && randomGeneratorForArray <= 75) {
@@ -309,9 +341,12 @@ brawl.testing.prototype = {
                             positionArray.push(bottomLeft);
                             positionArray.push(topRight);
                         }
-                        else {
+                        else if (amountOfSpritesInGrid === 2) {
                             positionArray.push(center);
                             positionArray.push(topRight);
+                        }
+                        else {
+                            positionArray.push(center);
                         }
                     }
                     else if (randomGeneratorForArray >= 76 && randomGeneratorForArray <= 100) {
@@ -320,9 +355,12 @@ brawl.testing.prototype = {
                             positionArray.push(bottomLeft);
                             positionArray.push(topRight);
                         }
-                        else {
+                        else if (amountOfSpritesInGrid === 2) {
                             positionArray.push(center);
                             positionArray.push(topRight);
+                        }
+                        else {
+                            positionArray.push(center);
                         }
                     }
 
@@ -336,6 +374,7 @@ brawl.testing.prototype = {
             }
         }
     },
+    /////////////////////////Randomness of the Map///////////////////////////
     gridSystem: function (x, y, rect, positionInRectangle) {
         //Create Randomness in Each Grid
         var gridSystemGenesis = this.game.rnd.integerInRange(0, 100);
@@ -392,7 +431,7 @@ brawl.testing.prototype = {
         // this.wallX.scale.setTo(.5);
         // var wallLength = [.2, .3, .4];
         // this.wallX.scale.setTo(wallLength[Math.floor(Math.random() * wallLength.length)]);
-        this.wallX.scale.setTo(.35);
+        this.wallX.scale.setTo(.5);
         this.wallX.body.collideWorldBounds = true;
         this.wallX.body.bounce.setTo(1);
         this.wallX.alignIn(rect, positionInRectangle)
