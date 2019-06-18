ab97547ae451b0cf81bbf8b59e0a724dfef79d6b
Luis Rishi Puno <LRPuno@gmail.com>
2019-06-18 08:02:29 -0400

    figured it f out

--- js/rogue.js
+++ js/rogue.js
@@ -580,11 +580,31 @@ brawl.rogue.prototype = {
     },
     ////////////////////////////////////////Localized Physics Functions//////////////////////////////////////
     //Enemy BUllets
-    fireEnemyBullet: function() {
+    fireEnemyBullet: function () {
+        // livingEnemies.length = 0; 
+        // this.enemy.forEachAlive(function(enemy){
+        //     livingEnemies.push(enemy)
+        // });
+        // if(this.time.now > enemyBulletTime) { 
+        //     enemyBullet = this.enemyBullets.getFirstExists(false); 
+        //     if(enemyBullet && livingEnemies.length > 0) {
+        //         //enemyShotSound.play();
+        //         var random = this.rnd.integerInRange(0, livingEnemies.length - 1);
+        //         var shooter = livingEnemies[random];
+        //         enemyBullet.reset(shooter.body.x, shooter.body.y + 30);
+        //         enemyBulletTime = this.time.now + 100; //500 was the "default value"
+        //         // if (game.physics.arcade.distanceBetween(enemyBullet, this.player, false, true) < 500) {
+        //         //     this.physics.arcade.moveToObject(enemyBullet,this.player,600);
+        //         // }
+        //         this.physics.arcade.moveToObject(enemyBullet,this.player,600);
+        //     }
+        // }
         livingEnemies.length = 0; 
         this.enemy.forEachAlive(function(enemy){
-            livingEnemies.push(enemy)
-        });
+            if (this.game.physics.arcade.distanceBetween(enemy, this.player, false, true) < 500) {
+                livingEnemies.push(enemy)
+            }
+        }, this, this.player);
         if(this.time.now > enemyBulletTime) { 
             enemyBullet = this.enemyBullets.getFirstExists(false); 
             if(enemyBullet && livingEnemies.length > 0) {
@@ -592,7 +612,10 @@ brawl.rogue.prototype = {
                 var random = this.rnd.integerInRange(0, livingEnemies.length - 1);
                 var shooter = livingEnemies[random];
                 enemyBullet.reset(shooter.body.x, shooter.body.y + 30);
-                enemyBulletTime = this.time.now + 500;
+                enemyBulletTime = this.time.now + 500; //500 was the "default value"
+                // if (game.physics.arcade.distanceBetween(enemyBullet, this.player, false, true) < 500) {
+                //     this.physics.arcade.moveToObject(enemyBullet,this.player,600);
+                // }
                 this.physics.arcade.moveToObject(enemyBullet,this.player,600);
             }
         }
