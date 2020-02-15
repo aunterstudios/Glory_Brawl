1b016ed3ca13351fd61be787293bf13e9f0bdaaa
Luis Rishi Puno <LRPuno@gmail.com>
2020-02-15 16:00:44 -0500

    separated weapon js

---
+++ js/weapon.js
@@ -0,0 +1,49 @@
+//////////////////////////////////////////Weapon Functionality////////////////////////////////////////////
+//When Weapon Hits Immovable/Unkillable Objects (It Dies);
+brawl.game.prototype.weaponImmovable = function (weapon, wall) {
+    weapon.kill();
+};
+//When Weapon Hits Moveable Objects (It's Special Property Expressed)
+brawl.game.prototype.weaponHandler = function (weapon, sprite) {
+    if (weapon.key === 'bulletPull') {
+        this.game.physics.arcade.moveToObject(sprite, this.player, 200);
+    }
+    else if (weapon.key === 'bulletStop') {
+        sprite.body.stop();
+    }
+    else if (weapon.key === 'bulletKill') {
+        sprite.kill();
+    }
+    weapon.kill();
+};
+//Let Weapon Fire Pass Through
+brawl.game.prototype.weaponGhost = function (weapon, ghost) {
+    if (ghost.name === wallCloud) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+///////////////////////////Weapon Switching/////////////////
+brawl.game.prototype.goPull = function () {
+    // console.log("1");
+    pullBoolean = true;
+    pushBoolean = false;
+    stopBoolean = false;
+    // console.log("Pull: " + pullBoolean + " Push: " + pushBoolean + " Kill: " + stopBoolean);
+};
+brawl.game.prototype.goPush = function () {
+    // console.log("2");
+    pullBoolean = false;
+    pushBoolean = true;
+    stopBoolean = false;
+    // console.log("Pull: " + pullBoolean + " Push: " + pushBoolean + " Kill: " + stopBoolean);
+};
+brawl.game.prototype.goKill = function () {
+    // console.log("3");
+    pullBoolean = false;
+    pushBoolean = false;
+    stopBoolean = true;
+    // console.log("Pull: " + pullBoolean + " Push: " + pushBoolean + " Kill: " + stopBoolean);
+};
