3ec6fe0bb731076f3c352290b6205b3da3ed7409
Luis Rishi Puno <LRPuno@gmail.com>
2020-08-14 07:48:59 -0400

    new JS file for compartementalization

---
+++ js/globalVariables.js
@@ -0,0 +1,319 @@
+//////////////////////////////////////////////////Global Variables//////////////////////////////////////////////
+//Weapon Variables to Change Bullet Type
+var pullBoolean = false;
+var pushBoolean = false;
+var stopBoolean = false;
+var killBoolean = false;
+
+//Respawn Holder (The Level You Will Respawn In)
+var respawnHolder = {
+  indexOfCurrentWorld: 2,
+  indexOfPlayerPosition: 3,
+  metroidvania: null,
+}
+
+//Toggling Camera
+var cameraBoolean = true;
+
+//Engage Coordinate system
+
+var coordinateSystem = true;
+// var coordinateSystem = false;
+
+// Global Timer
+var total = 0;
+
+//BMD Text (Toggle On or Off)
+// var bitmapBoolean = true;
+var bitmapBoolean = false;
+//BMD Font
+var fontGrind = 'fontGrind';
+
+//Slow Motion
+var slowMotionLimit;
+var timerEvents;
+
+//Frame Rate 
+// var setFps;
+
+// Total Deaths
+var deaths = 0;
+
+/////////////////////////Player Attributes/////////////////////////(Can Be Used For Later Things)
+//Remember All These Things Are changed in the Init Function of game.js
+var playerSpeed;
+var playerJump;
+var playerGravity;
+var playerDoubleJumps;
+var playerWallJumpX;
+var playerWallJumpY;
+var playerWallDisengage;
+var playerStickiness;
+var playerSlippery;
+var playerUpsideDownVelocity;
+var playerUpsideDownMovement;
+var playerDownwards;
+
+/////////////////////////Weapon Attributes////////////
+var weaponFireRate;
+var weaponBulletSpeed;
+var weaponBulletAmount;
+
+
+/////////////////////////List of GROUP NAMES of Each Sprite (For Different Special Properties)////////////////
+/*
+Note: This is an extra name that denotes the entirety of a group. To keep physics consistent throughout.
+*/
+//Death Group
+var groupUndeniableDeath = 'groupUndeniableDeath';
+//Spikes
+var groupSpikes = 'groupSpikes';
+//Immovable Walls
+var groupImmovableWall = 'groupImmovableWall';
+//Moveable Walls
+var groupWall = 'groupWall';
+//Ledge
+var groupLedge = 'groupLedge';
+//Enemy
+var groupEnemy = 'groupEnemy';
+//Ball
+var groupBall = 'groupBall';
+//Falling Spikes
+var groupFallingSpikes = 'groupFallingSpikes';
+//Flag
+var groupFlag = 'groupFlag';
+
+/////////////////////////List of Names of Each Sprite (For Different Special Properties)////////////////
+//Death Names
+var undeniableDeathRegular = 'undeniableDeathRegular';
+//Spikes
+var spikesRegular = 'spikesRegular'
+
+//Immovable Wall Names
+var immovableWallRegular = 'immovableWallRegular'; //No Special Properties
+var immovableWallKillWall = 'immovableWallKillWall'; //Kills Walls(Will Be Everything)
+var immovableWallPhase = 'immovableWallPhase'; //Killed By Enemy BULLETS
+var immovableWallActivation = 'immovableWallActivation'; //Triggers Movement in a Wall
+var immovableWallWorldGravity = 'immovableWallWorldGravity'; //Triggers World Gravity
+var immovableWallOneWayObject = 'immovableWallOneWayObject'; //One Way (Objects Only)
+var immovableWallOneWayPlayer = 'immovableWallOneWayPlayer'; //One Way (Players Only)
+var immovableWallSlippery = 'immovableWallSlippery'; //Makes you SLIPPERY!
+var immovableWallOneWayPlayerBlockLeft = 'immovableWallOneWayPlayerBlockLeft'; //One way player only from the left
+
+//Moveable Wall Names
+var wallRegular = 'wallRegular';
+var wallPlayerFrozen = 'wallPlayerFrozen';
+var wallSurf = 'wallSurf';
+var wallInverse = 'wallInverse'; //First Turn (Leaners Walls From Ledge)
+var wallGhost = 'wallGhost'; //Immovable Wall That Let's You Get Through Objects
+var wallCloud = 'wallCloud'; //Stationary Shooting Platform Cloud
+//Ledge Names
+var elevator = 'elevator';
+var bounce = 'bounce';
+var surf = 'surf';
+
+//Enemy Names
+var enemyShooter = 'enemyShooter';
+var enemyDaakath = 'enemyDaakath';
+var enemyAccelerate = 'enemyAccelerate';
+
+//Ball Names
+var ballRegular = 'ballRegular';
+
+//Flag Names
+var flagRegular = 'flagRegular';
+var flagSpecial = 'flagSpecial';
+
+/////////////////////////List of Art of Each Image/////////////////
+var star = 'star';
+
+/////////////////////////List of Art of Each Sprite/////////////////
+//Death
+var deathTile = 'deathTile';
+
+//Immovable Wall
+var immovableWallTile = 'immovableWallTile';
+
+//Moveable Wall
+var wallTile50 = 'wallTile50';
+var wallTile25 = 'wallTile25';
+
+//Ledge
+var ledge = 'ledge';
+
+//enemy
+var enemyOne = 'enemyOne'
+
+//Ball
+var ball = 'ball';
+
+//Falling Spikes
+var spikeFall = 'spikeFall';
+
+//Flag
+var flag = 'flag';
+
+//Slow Motion
+var slowMotion = 'slowMotion';
+///////////////////////////////////////////Tint Specific Art//////////////////////////////////////////////
+//Tint Remover 
+var tintRemover = 0xFFFFFF; //wallRegular (Removes Tint)
+//Immovable Walls
+var tintImmovableWallKillWall = 7019278.306799905;
+var tintImmovableWallPhase = 15631118.030252509;
+var tintImmovableWallMagnet = 10804989.680595484;
+var tintImmovableWallActivation = 0xffff00;
+var tintImmovableWallPadding = 2499878.036284214;
+var tintImmovableWallWorldGravity = 8314793.039214706;
+var tintImmovableWallMario = 241917.63554178402;
+var tintImmovableWallSlippery = 766012.4141677661;
+var tintImmovableWallOneWayPlayerBlockLeft = 3588771.242333334;
+
+//Frozen Wall Tints
+var tintWallPlayerFrozen = 0x00ffff;
+//Surf Wall Tints
+var tintWallSurf = 10409939.733364154;
+//Inverse Tints
+var tintWallInverse = 1181911.9307258818;
+//Ghost Tints
+var tintWallGhost = 16771007.229130682;
+//Special Wall Tints
+var tintWallCloud = 9583870.358153213;
+
+//Enemies
+var tintEnemyShooter = 12758247.409111453;
+var tintEnemyDaakath = 15269906.933038201;
+var tintEnemyAccelerate = 2885804.4944837275;
+
+
+////////////////////////////////////////////Designed Levels/////////////////////////////////////////////////////
+//Level Holder
+var worldClassLevels = [];
+//Creates Each Individual Level
+class LevelCreator {
+  constructor(worldName, xOfWorld, yOfWorld, metroidvania, backgroundColor) {
+    this.worldName = worldName;
+    this.xOfWorld = xOfWorld;
+    this.yOfWorld = yOfWorld;
+    this.metroidvania = metroidvania;
+    this.backgroundColor = backgroundColor;
+  }
+};
+
+///Creates Room Switching
+class MetroidvaniaCreator {
+  constructor(roomUpIndex, roomUpValue, roomDownIndex, roomDownValue, roomLeftIndex, roomLeftValue, roomRightIndex, roomRightValue) {
+    this.roomUpIndex = roomUpIndex;
+    this.roomUpValue = roomUpValue
+    this.roomDownIndex = roomDownIndex;
+    this.roomDownValue = roomDownValue;
+    this.roomLeftIndex = roomLeftIndex;
+    this.roomLeftValue = roomLeftValue;
+    this.roomRightIndex = roomRightIndex;
+    this.roomRightValue = roomRightValue;
+  }
+};
+
+//Creates Player Positioning (Up, Down, Left Right);
+class PlayerPositionCreator {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+};
+
+//Creates World Gravity Object
+class worldGravityCreator {
+  constructor(gravityX, gravityY) {
+    this.gravityX = gravityX;
+    this.gravityY = gravityY;
+  }
+};
+
+//Generates All Physics Sprites
+class SpriteCreator {
+  constructor(trigger, name, art, x, y, widthX, widthY, scale, velocityX, velocityY, gravityX, gravityY, specialCondition, seconds) {
+    this.trigger = trigger;
+    this.name = name;
+    this.art = art;
+    this.x = x;
+    this.y = y;
+    this.widthX = widthX;
+    this.widthY = widthY;
+    this.scale = scale;
+    this.velocityX = velocityX;
+    this.velocityY = velocityY;
+    this.gravityX = gravityX;
+    this.gravityY = gravityY;
+    this.specialCondition = specialCondition;
+    this.seconds = seconds;
+  }
+};
+
+//Creates Images
+class imageCreator {
+  constructor(trigger, name, art, x, y, scale) {
+    this.trigger = trigger;
+    this.name = name;
+    this.art = art;
+    this.x = x;
+    this.y = y;
+    this.scale = scale;
+  }
+}
+
+//////////////////Flag Specific Classes/////////
+class flagCreator {
+  constructor(indexOfPlayerPosition, trigger, name, art, x, y, velocityX, velocityY, sizeX, sizeY, gravityX, gravityY, specialHandler) {
+    this.indexOfPlayerPosition = indexOfPlayerPosition;
+    this.trigger = trigger;
+    this.name = name;
+    this.art = art;
+    this.x = x;
+    this.y = y;
+    this.velocityX = velocityX;
+    this.velocityY = velocityY;
+    this.sizeX = sizeX;
+    this.sizeY = sizeY;
+    this.gravityX = gravityX;
+    this.gravityY = gravityY;
+    this.specialHandler = specialHandler;
+  }
+};
+
+////////////////////////////////////Experimental////////////////////////////////
+// class specialHandler {
+//   constructor(specialWorld, undeniableDeathInsert, undeniableDeathRemove, immovableWallInsert, immovableWallRemove, wallInsert, wallRemove, ledgeInsert,ledgeRemove, ballInsert, ballRemove, enemyInsert, enemyRemove, spikeFallInsert, spikeFallRemove, flagInsert, flagRemove, textInsert, textRemove) {
+//     this.specialWorld = specialWorld;
+//     this.undeniableDeathInsert = undeniableDeathInsert;
+//     this.undeniableDeathRemove = undeniableDeathRemove;
+//     this.immovableWallInsert = immovableWallInsert;
+//     this.immovableWallRemove = immovableWallRemove;
+//     this.wallInsert = wallInsert;
+//     this.wallRemove = wallRemove;
+//     this.ledgeInsert = ledgeInsert;
+//     this.ledgeRemove = ledgeRemove;
+//     this.ballInsert = ballInsert;
+//     this.ballRemove = ballRemove;
+//     this.enemyInsert = enemyInsert;
+//     this.enemyRemove = enemyRemove;
+//     this.spikeFallInsert = spikeFallInsert;
+//     this.spikeFallRemove = spikeFallRemove;
+//     this.flagInsert = flagInsert;
+//     this.flagRemove = flagRemove;
+//     this.textInsert = textInsert;
+//     this.textRemove = textRemove;
+//   }
+// };
+
+//Creating Bitmap Text Class
+class textCreator {
+  constructor(trigger, x, y, textInput, font, fontSize) {
+    this.trigger = trigger;
+    this.x = x;
+    this.y = y;
+    this.textInput = textInput;
+    this.font = font;
+    this.fontSize = fontSize;
+  }
+}
