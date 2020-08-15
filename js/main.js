8d9f8a8834b55f08d9410e0dc7f5c64f5f546f46
Luis Rishi Puno <LRPuno@gmail.com>
2020-08-15 12:37:16 -0400

    preload all assets omg

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ game.state.add('controlScreen', brawl.stateControls);
 game.state.start('mainMenu');
 //////////////////////////////////////////////////Main Menu Story//////////////////////////////////////////////
 var content = [
-  "RefactoredRemoveConsoleLog",
+  "PreloadAssets",
   "GLORY BRAWL",
 ];
 
