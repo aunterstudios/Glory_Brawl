33ee03f3350ba71fd728fc3f37692f68806f7cd6
Luis Rishi Puno <LRPuno@gmail.com>
2020-10-30 07:51:19 -0400

    queens gambit

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ game.state.add('startScreen', brawl.startScreen);
 game.state.start('boot');
 //////////////////////////////////////////////////Main Menu Story//////////////////////////////////////////////
 var content = [
-  "BrowserPauseREMOVED",
+  "Level2",
   "GLORY BRAWL",
 ];
 
