353d8b55359e2d6a00b39d70ae1a1a56fc502bd9
Luis Rishi Puno <LRPuno@gmail.com>
2020-11-03 07:40:17 -0500

    lerp

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ game.state.add('startScreen', brawl.startScreen);
 game.state.start('boot');
 //////////////////////////////////////////////////Main Menu Story//////////////////////////////////////////////
 var content = [
-  "Balance Issues",
+  "Can't go back now",
   "GLORY BRAWL",
 ];
 
