b2e6d905084a1d80046b27c7ddf0f593d7943b05
Luis Rishi Puno <LRPuno@gmail.com>
2020-11-03 23:14:49 -0500

    darker background

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ game.state.add('startScreen', brawl.startScreen);
 game.state.start('boot');
 //////////////////////////////////////////////////Main Menu Story//////////////////////////////////////////////
 var content = [
-  "Turned It Down",
+  "Darker Background",
   "GLORY BRAWL",
 ];
 
